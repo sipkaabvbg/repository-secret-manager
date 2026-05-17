06cb6004dea8671336ccaf2a4013f5936b881c93
Alexander Ivanov <sipka@abv.bg>
2026-05-17 15:42:48 +0300

    initialize repository-secret-service project with Java 25 and Spring Boot 3.5.14

---
+++ repository-secret-service/src/test/java/com/cicd/repository_secret_service/RepositorySecretServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.repository_secret_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorySecretServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
