af23fc2d4d43b643e546b0cd07b404e29e17e229
Alexander Ivanov <sipka@abv.bg>
2026-05-17 17:29:11 +0300

    initialize repository-secret-ui frontend application

---
+++ repository-secret-ui/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("repository.secret.ui.controller.App", {
+        onInit: function () {
+            // 
+        }
+    });
+});
