76b6971c079982b3cbe353fbbc94157b4b80ed69
Alexander Ivanov <sipka@abv.bg>
2026-05-18 11:48:56 +0300

    (#11) Implement ExternalRepoService and SecretService for business logic

--- repository-secret-service/src/main/java/com/cicd/credentials/repository/ExternalRepoRepository.java
+++ repository-secret-service/src/main/java/com/cicd/credentials/repository/ExternalRepoRepository.java
@@ -10,5 +10,5 @@ import org.springframework.stereotype.Repository;
  * Provides standard CRUD operations and database interaction capabilities thanks to Spring Data JPA.
  */
 @Repository
-public interface GitRepoRepository extends JpaRepository<ExternalRepoEntity, Long> {
+public interface ExternalRepoRepository extends JpaRepository<ExternalRepoEntity, Long> {
 }
